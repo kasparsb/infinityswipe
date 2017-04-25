2330f580802ce8301599c42235fc922a185313e6
Kaspars Bulins <kasparsb@hotmail.com>
2017-04-26 02:09:00 +0300

    Handle slide add parcelts arpus swipe

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,53 @@
 var $ = require('jquery');
-var swipe = require('./swipe');
+var Infty = require('./swipe');
 
-swipe.init($('.swipe--1').get(0));
-swipe.init($('.swipe--2').get(0));
-swipe.init($('.swipe--3').get(0));
-swipe.init($('.swipe--4').get(0));
+var api1 = new Infty($('.swipe--1').get(0));
+var api2 = new Infty($('.swipe--2').get(0));
+var api3 = new Infty($('.swipe--3').get(0));
+var api4 = new Infty($('.swipe--4').get(0));
+
+
+var imageIndex1 = 0, imageIndex2 = 0, imageIndex3 = 0, imageIndex4 = 0;
+var images = [
+    'https://c1.staticflickr.com/3/2859/32924770393_384a76c13e.jpg',
+    'https://c1.staticflickr.com/3/2901/33605527231_b6c94cac82.jpg',
+    'https://c1.staticflickr.com/3/2832/32919038623_2c6eb8c023.jpg',
+    'https://c1.staticflickr.com/3/2877/33613662191_09caa3f6a3.jpg',
+    'https://c1.staticflickr.com/3/2822/32918826513_3bc7b17908.jpg',
+    'https://c2.staticflickr.com/4/3934/33582408212_5b9d226183.jpg',
+    'https://c2.staticflickr.com/4/3669/33737824555_2d8f446c36.jpg',
+    'https://c1.staticflickr.com/3/2878/33732917345_daaa775e98.jpg',
+    'https://c1.staticflickr.com/3/2835/32886081974_36a9531d7b.jpg',
+    'https://c2.staticflickr.com/4/3804/32895132024_a7679bcc88.jpg',
+    'https://c1.staticflickr.com/3/2858/33577553782_c7db967deb.jpg',
+    'https://c2.staticflickr.com/4/3734/33699026186_436c82bcbc.jpg'
+];
+
+function addImage(imageIndex, el) {
+    $(el).css({
+        'background-image': 'url('+images[imageIndex]+')',
+        'background-size': 'cover',
+        'background-position': 'center'
+    });
+
+    imageIndex++;
+    if (imageIndex >= images.length) {
+        imageIndex = 0;
+    }
+
+    return imageIndex;
+}
+
+
+api1.onSlideAdd(function(index, el){
+    imageIndex1 = addImage(imageIndex1, el);
+});
+api2.onSlideAdd(function(index, el){
+    imageIndex2 = addImage(imageIndex2, el);
+});
+api3.onSlideAdd(function(index, el){
+    imageIndex3 = addImage(imageIndex3, el);
+});
+api4.onSlideAdd(function(index, el){
+    imageIndex4 = addImage(imageIndex4, el);
+});
--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -3,12 +3,10 @@ var Swipe = require('swipe');
 var Slides = require('./slides');
 var Stepper = require('./stepper');
 
-var B1 = require('./bezier1');
-var B2 = require('./bezier1');
-
-
 function createSwipe(el) {
-    var $el, sw, slides, stepper, viewportWidth;
+
+    var slideAddCb;
+    var sw, slides, stepper, viewportWidth;
     var startPos = 0, offsetX = 0, isMoveStarted = false;
 
     //var stepperCurve = [0,0,1,1]; // linear
@@ -17,34 +15,8 @@ function createSwipe(el) {
     var stepperCurve = [0,0,.12,1];
     var stepperDuration = 300;
 
-
-    var imageIndex = 0;
-    var images = [
-        'https://c1.staticflickr.com/3/2859/32924770393_384a76c13e.jpg',
-        'https://c1.staticflickr.com/3/2901/33605527231_b6c94cac82.jpg',
-        'https://c1.staticflickr.com/3/2832/32919038623_2c6eb8c023.jpg',
-        'https://c1.staticflickr.com/3/2877/33613662191_09caa3f6a3.jpg',
-        'https://c1.staticflickr.com/3/2822/32918826513_3bc7b17908.jpg',
-        'https://c2.staticflickr.com/4/3934/33582408212_5b9d226183.jpg',
-        'https://c2.staticflickr.com/4/3669/33737824555_2d8f446c36.jpg',
-        'https://c1.staticflickr.com/3/2878/33732917345_daaa775e98.jpg',
-        'https://c1.staticflickr.com/3/2835/32886081974_36a9531d7b.jpg',
-        'https://c2.staticflickr.com/4/3804/32895132024_a7679bcc88.jpg',
-        'https://c1.staticflickr.com/3/2858/33577553782_c7db967deb.jpg',
-        'https://c2.staticflickr.com/4/3734/33699026186_436c82bcbc.jpg'
-    ];
-
-
-    function log(message) {
-        // $.post('http://webing.local:8080/api/ping/debug', {
-        //     description: message
-        // });
-
-        //console.log(message);
-    }
-
     function initSwipe() {
-        sw = new Swipe($el.get(0), {'direction': 'horizontal'})
+        sw = new Swipe(el, {'direction': 'horizontal'})
             .on('start', startMove)
             .on('move', handleMove)
             // Notiek, tikai, kad ir bijis valid move
@@ -57,7 +29,7 @@ function createSwipe(el) {
     }
 
     function initSlides() {
-        slides = new Slides($el.get(0), {
+        slides = new Slides(el, {
             onSlideAdd: handleSlideAdd
         });
     }
@@ -75,9 +47,7 @@ function createSwipe(el) {
         isMoveStarted = true;
     }
 
-    function handleMove(d) {
-        console.log('move', d.direction);
-        
+    function handleMove(d) {    
         if (stepper.isRunning()) {
             return;
         }
@@ -91,8 +61,6 @@ function createSwipe(el) {
 
     function endMove(d) {
         var x = findSlideOffsetX(0, viewportWidth);
-
-        console.log('endmove', x, d.direction);
 
         if (typeof x == 'undefined') {
             return;
@@ -146,8 +114,6 @@ function createSwipe(el) {
          */
         slides.start();
         
-        console.log('startprogress', startProgress, d.direction, d.isSwipe ? 'swipe' : 'notswipe');
-        
         stepper.runFrom(startProgress, stepperDuration, stepperCurve, function(progress){
             
             // Šis būs tas offset, kurš tiek animēts un kurš ir jāliek klāt slaidam
@@ -162,27 +128,11 @@ function createSwipe(el) {
                 targetOffset = (x - targetOffset) * -1;
             }
 
-            //console.log(progress, targetOffset, x, slideMoveDirection);
-
             slides.setXOffset(targetOffset);
 
         }, function(){
             // Done
         })
-    }
-
-    function handleSlideAdd(index, el) {
-        
-        $(el).css({
-            'background-image': 'url('+images[imageIndex]+')',
-            'background-size': 'cover',
-            'background-position': 'center'
-        });
-
-        imageIndex++;
-        if (imageIndex >= images.length) {
-            imageIndex = 0;
-        }
     }
 
     function findSlideOffsetX(start, stop) {
@@ -195,19 +145,37 @@ function createSwipe(el) {
     }
 
 
+    function handleSlideAdd(index, el) {
+        if (slideAddCb) {
+            slideAddCb(index, el);
+        }
+    }
 
-    $el = $(el);
 
-    viewportWidth = $el.width();
+    viewportWidth = $(el).width();
     
-    initSwipe();
-    initSlides();
-    initStepper();
+    // Liekam timeout, lai izpildās nākamajā scope
+    // Vajag, lai izsaucošais kods var uzlikt onSlideAdd pirms tam
+    setTimeout(function(){
+        initSwipe();
+        initSlides();
+        initStepper();
+    });
+
+    return {
+        onSlideAdd: function(cb) {
+            slideAddCb = cb;
+        },
+        nextSlide: function() {
+            console.log('slidenext', el)
+        },
+        prevSlide: function() {
+            console.log('slideprev', el)
+        }
+    }
 }
 
 
-module.exports = {
-    init: function(el) {
-        createSwipe(el);
-    }
+module.exports = function(el) {
+    return createSwipe(el);
 }
